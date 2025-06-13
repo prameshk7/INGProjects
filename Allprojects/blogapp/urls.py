666f0380855c18be082a1edb9bd7b5f2692bff58
prameshk7 <pramesh.katuwal.7@gmail.com>
2025-06-13 12:35:33 +0545

    Updated the URL to add authentication

---
+++ Allprojects/blogapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='list'),
+    path('post/<int:id>/', views.post_detail, name='detail'),
+    path('post/new/', views.post_create, name='create'),
+    path('post/<int:id>/edit/', views.post_update, name='update'),
+    path('post/<int:id>/delete/', views.post_delete, name='delete'),
+    path('login/', views.user_login, name='login'), 
+    path('logout/', views.user_logout, name='logout'),
+]
+
