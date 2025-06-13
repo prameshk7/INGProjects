eae62e77883043e1c3969fa62a2dea1aee181a6e
prameshk7 <pramesh.katuwal.7@gmail.com>
2025-06-13 12:40:43 +0545

    Added user registration form in blogapp

--- Allprojects/blogapp/forms.py
+++ Allprojects/blogapp/forms.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .models import Post
 
@@ -15,5 +17,18 @@ class PostForm(forms.ModelForm):
         if not content or not content.strip():
             raise forms.ValidationError("Content cannot be empty.")
         return input_data
-    
+ 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)  # Add email field
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    # Basic validation for email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already in use.")
+        return email   
     
