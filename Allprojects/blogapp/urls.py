b5e06d32644a9fb0941c1c9757644fcbc14ca9a7
prameshk7 <pramesh.katuwal.7@gmail.com>
2025-06-13 12:44:55 +0545

    Created registration templates fro blogapp

--- Allprojects/blogapp/urls.py
+++ Allprojects/blogapp/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('post/<int:id>/delete/', views.post_delete, name='delete'),
     path('login/', views.user_login, name='login'), 
     path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'), 
 ]
 
