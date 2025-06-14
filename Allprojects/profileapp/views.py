df7cbaddb4394aadbcc61a2a7d6d484cbf4fa05c
prameshk7 <pramesh.katuwal.7@gmail.com>
2025-06-14 08:20:51 +0545

    Implemented registration logic in task-3 profileapp

--- Allprojects/profileapp/views.py
+++ Allprojects/profileapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordResetForm, SetPasswordForm
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
+from .forms import ProfileForm
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful. Redirecting to login...')
+            return redirect('userapp:login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'userapp/register.html', {'form': form})
